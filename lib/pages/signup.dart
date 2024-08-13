a906985fef661a2d14aa6a4834568c521210fc3e
Teertho <raahim.rubaiyat2021@gmail.com>
2024-08-13 22:23:07 +0600

    added user detail adding

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app_project/main.dart';
@@ -13,6 +14,11 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _ageController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
   bool _obscureText = true;
   String? _errorMessage;
   bool _isLoading = false;
@@ -30,22 +36,33 @@ class _SignupState extends State<Signup> {
     });
 
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const MyApp(),
-        ),
-      );
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('User created successfully, login now'),
-          duration: Duration(seconds: 2),
-        ),
-      );
+      if (passwordMatches()) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+
+        addUserDetails(
+            _firstNameController.text.trim(),
+            _lastNameController.text.trim(),
+            _emailController.text.trim(),
+            int.parse(_ageController.text.trim()));
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MyApp(),
+          ),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('User created successfully, login now'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      } else {
+        _errorMessage = "The passwords do not match";
+      }
     } catch (e) {
       setState(() {
         _errorMessage = e.toString();
@@ -55,6 +72,23 @@ class _SignupState extends State<Signup> {
         _isLoading = false;
       });
     }
+  }
+
+  bool passwordMatches() {
+    return _passwordController.text.trim() ==
+        _confirmPasswordController.text.trim();
+  }
+
+  Future addUserDetails(
+      String firstName, String lastName, String email, int age) async {
+    await FirebaseFirestore.instance.collection('users').add({
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      'age': age,
+      'todo': [{}],
+      'done': [{}]
+    });
   }
 
   @override
@@ -69,106 +103,168 @@ class _SignupState extends State<Signup> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'SIGNUP',
-                style: TextStyle(
-                  fontSize: 45,
-                  color: Colors.white,
-                  letterSpacing: 4,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: const InputDecoration(
-                        hintText: 'Email',
-                        border: OutlineInputBorder(),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: const TextStyle(color: Colors.white),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'SIGNUP',
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: Colors.white,
+                      letterSpacing: 4,
                     ),
-                    const SizedBox(height: 30),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: _obscureText,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        border: const OutlineInputBorder(),
-                        hintStyle: const TextStyle(color: Colors.white),
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _obscureText = !_obscureText;
-                            });
-                          },
-                          icon: Icon(
-                            _obscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    const SizedBox(height: 20),
-                    if (_errorMessage != null)
-                      Text(
-                        _errorMessage!,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                    const SizedBox(height: 20),
-                    _isLoading
-                        ? const CircularProgressIndicator()
-                        : ElevatedButton(
-                            onPressed: () async {
-                              if (_emailController.text.isNotEmpty &&
-                                  _passwordController.text.isNotEmpty) {
-                                await signUp();
-                              } else {
-                                setState(() {
-                                  _errorMessage = 'Please enter all fields';
-                                });
-                              }
-                            },
-                            child: const Text('Signup'),
-                          ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Column(
                       children: [
-                        const Text(
-                          "Already a member?",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Login(),
-                              ),
-                            );
-                          },
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white),
+                        TextField(
+                          controller: _firstNameController,
+                          decoration: const InputDecoration(
+                            hintText: 'First Name',
+                            border: OutlineInputBorder(),
+                            hintStyle: TextStyle(color: Colors.white),
                           ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          controller: _lastNameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Last Name',
+                            border: OutlineInputBorder(),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _ageController,
+                          decoration: const InputDecoration(
+                            hintText: 'Age',
+                            border: OutlineInputBorder(),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: _obscureText,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            border: const OutlineInputBorder(),
+                            hintStyle: const TextStyle(color: Colors.white),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              icon: Icon(
+                                _obscureText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _confirmPasswordController,
+                          obscureText: _obscureText,
+                          decoration: InputDecoration(
+                            hintText: 'Confirm Password',
+                            border: const OutlineInputBorder(),
+                            hintStyle: const TextStyle(color: Colors.white),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              icon: Icon(
+                                _obscureText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 20),
+                        if (_errorMessage != null)
+                          Text(
+                            _errorMessage!,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        const SizedBox(height: 20),
+                        _isLoading
+                            ? const CircularProgressIndicator()
+                            : ElevatedButton(
+                                onPressed: () async {
+                                  if (_emailController.text.isNotEmpty &&
+                                      _passwordController.text.isNotEmpty) {
+                                    await signUp();
+                                  } else {
+                                    setState(() {
+                                      _errorMessage = 'Please enter all fields';
+                                    });
+                                  }
+                                },
+                                child: const Text('Signup'),
+                              ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Already a member?",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Login(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
